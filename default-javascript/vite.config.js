61aa902d894938e0348496f7d7d5bd370aba833c
Brooks Lybrand <brookslybrand@gmail.com>
2024-11-25 15:10:04 -0600

    remove tsconfigPaths and add entry files

--- default-javascript/vite.config.js
+++ default-javascript/vite.config.js
@@ -2,7 +2,6 @@ import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   css: {
@@ -10,5 +9,5 @@ export default defineConfig({
       plugins: [tailwindcss, autoprefixer],
     },
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter()],
 });
