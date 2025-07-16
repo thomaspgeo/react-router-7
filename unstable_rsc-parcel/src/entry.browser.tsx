2fb1d62477a1c3ffa36969419c4aa0921a0d6aa9
Jacob Ebey <jacob.ebey@live.com>
2025-07-16 11:59:02 -0700

    feat: unstable RSC templates (#139)
    Co-authored-by: Brooks Lybrand <brookslybrand@gmail.com>
    Co-authored-by: Hiroshi Ogawa <hi.ogawa.zz@gmail.com>

---
+++ unstable_rsc-parcel/src/entry.browser.tsx
@@ -0,0 +1,48 @@
+"use client-entry";
+
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+import {
+  unstable_createCallServer as createCallServer,
+  unstable_getRSCStream as getRSCStream,
+  unstable_RSCHydratedRouter as RSCHydratedRouter,
+  type unstable_RSCPayload as RSCServerPayload,
+} from "react-router";
+import {
+  createFromReadableStream,
+  createTemporaryReferenceSet,
+  encodeReply,
+  setServerCallback,
+  // @ts-expect-error - no types for this yet
+} from "react-server-dom-parcel/client";
+
+// Create and set the callServer function to support post-hydration server actions.
+setServerCallback(
+  createCallServer({
+    createFromReadableStream,
+    createTemporaryReferenceSet,
+    encodeReply,
+  }),
+);
+
+// Get and decode the initial server payload
+createFromReadableStream(getRSCStream()).then((payload: RSCServerPayload) => {
+  startTransition(async () => {
+    const formState =
+      payload.type === "render" ? await payload.formState : undefined;
+
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <RSCHydratedRouter
+          createFromReadableStream={createFromReadableStream}
+          payload={payload}
+        />
+      </StrictMode>,
+      {
+        // @ts-expect-error - no types for this yet
+        formState,
+      },
+    );
+  });
+});
