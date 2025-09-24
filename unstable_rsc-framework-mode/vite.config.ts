31cd7911cf13bb1dfc470537ff9219150188efc2
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2025-09-25 01:31:41 +1000

    WIP: Add unstable RSC Framework Mode template (#158)
    * Add unstable RSC Framework Mode template
    
    For consistency, rename RSC Data Mode templates
    
    * Update lockfile
    
    * Remove generated types, update gitignore
    
    * Convert home route to server component
    
    * simplify rsc framework template and use react-router-serve
    
    * update version
    
    * add vite-tsconfig-paths
    
    * remove comments
    
    * update version
    
    * Bump deps
    
    ---------
    
    Co-authored-by: Jacob Ebey <jacob.ebey@live.com>
    Co-authored-by: Brooks Lybrand <brookslybrand@gmail.com>

---
+++ unstable_rsc-framework-mode/vite.config.ts
@@ -0,0 +1,16 @@
+import { unstable_reactRouterRSC as reactRouterRSC } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import rsc from "@vitejs/plugin-rsc";
+import { defineConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    tsconfigPaths(),
+    reactRouterRSC(),
+    rsc(),
+    devtoolsJson(),
+  ],
+});
